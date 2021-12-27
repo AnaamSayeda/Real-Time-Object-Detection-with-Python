9bd5e08e7c38173f1fbc442afda7bbe24c68019e
Anaam <anamsayeda25@gmail.com>
2021-12-27 11:43:34 +0400

    Intial Commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('img.png')
+vid = cv2.VideoCapture(0)
+vid.set(3,800)
+vid.set(4,700)
+vid.set(10,70)
 classNames = []
 
 file = 'coco.names'
@@ -17,13 +20,17 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-classid, confidence, bbox = net.detect(img, confThreshold=0.5)
-print(classid, bbox)
 
-for id, conf, box in zip(classid.flatten(), confidence.flatten(), bbox):
-    cv2.rectangle(img, box, color=(255, 255, 0), thickness=5)
-    cv2.putText(img, classNames[id - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_PLAIN, 1,
-                (255, 255, 0), 2)
+while True:
+    success, img = vid.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    print(classIds, bbox)
 
-cv2.imshow('output', img)
-cv2.waitKey(0)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+
+    cv2.imshow('output', img)
+    cv2.waitKey(0)
